53da2fc73a218334b81c831e06f355d7c1bdb6e7
tranquyenbk173 <tranquyenhd17@gmail.com>
2024-09-18 14:13:52 +0000

    f

--- training_scripts/train_cifar100_vit.sh
+++ training_scripts/train_cifar100_vit.sh
@@ -18,9 +18,15 @@
 #         --lr 0.0005 
 # done
 
+reg=0.01
+reg_sub=0.1
+reg_glob=0.05
+prompt_momentum=0.0001
+lr=0.03
+
 for seed in 422
 do
-CUDA_VISIBLE_DEVICES=6 python -m torch.distributed.launch \
+CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.launch \
 	--nproc_per_node=1 \
 	--master_port='29503' \
 	--use_env main.py \
@@ -30,18 +36,18 @@ CUDA_VISIBLE_DEVICES=6 python -m torch.distributed.launch \
 	--batch-size 24 \
 	--epochs 50 \
 	--data-path ../Z.Data/ \
-	--lr 0.03 \
+	--lr $lr \
 	--ca_lr 0.005 \
 	--crct_epochs 30 \
 	--seed $seed \
-	--prompt_momentum 0.0001 \
-	--reg 0.01 \
-	--reg_sub 0.1 \
-	--reg_glob 0.05 \
+	--prompt_momentum $prompt_momentum \
+	--reg $reg \
+	--reg_sub $reg_sub \
+	--reg_glob $reg_glob \
 	--order 1 \
 	--length 5 \
 	--sched step \
 	--larger_prompt_lr \
 	--trained_original_model ./output/cifar100_sup21k_multi_centroid_mlp_2_seed42 \
-	--output_dir ./output/cifar100_vit_pe_seed$seed-_reg$reg-regsub$reg_regsub-regglob$reg_glob-prompt_momentum$prompt_momentum-lr$lr
+	--output_dir ./output/cifar100_vit_pe_seed${seed}-reg${reg}-regsub${reg_sub}-regglob${reg_glob}-prompt_momentum${prompt_momentum}-lr${lr}
 done
